6175395e661e624100d6ab44d8eaee07811929c7
GiovanniWiseDream <giovannijimenezmurciano@gmail.com>
2024-11-19 22:15:29 +0000

    Arreglo de home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -22,8 +22,8 @@ function Home() {
   ];
 
   return (
-    <div className="flex justify-center items-start h-full w-full bg-light-purple overflow-y-auto overflow-x-hidden">
-      <div className="w-5/6 flex flex-col items-center justify-center ">
+    <div className="flex justify-center items-center h-full w-full max-h-dvh bg-light-purple overflow-y-auto overflow-x-hidden sm:max-h-none">
+      <div className="w-5/6 md:w-4/5 h-full max-h-[90dvh] flex flex-col items-center justify-start">
         <header className="text-center w-full p-4 rounded mb-4">
           <h1 className="pb-3">Aniview</h1>
           <input
@@ -34,7 +34,7 @@ function Home() {
           />
         </header>
 
-        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <section className=" pb-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           <div className=" bg-white rounded shadow max-h-[70vh] overflow-hidden ">
             <div className=" w-full z-10 h-fit bg-white drop-shadow-lg">
               <h1 className="font-bold text-center py-4 text-2xl">Top Anime</h1>
@@ -55,12 +55,11 @@ function Home() {
             </div>
 
             <div className="space-y-4 p-4 overflow-auto max-h-[60vh]">
-              {/* Overflow en el contenedor interno */}{" "}
               {animes.map((anime, index) => (
                 <AnimeCard key={index} anime={anime} />
               ))}
             </div>
-          </div>{" "}
+          </div>
           <div className=" bg-white rounded shadow max-h-[70vh] overflow-hidden ">
             <div className=" w-full z-10 h-fit bg-white drop-shadow-lg">
               <h1 className="font-bold text-center py-4 text-2xl">
@@ -69,7 +68,6 @@ function Home() {
             </div>
 
             <div className="space-y-4 p-4 overflow-auto max-h-[60vh]">
-              {/* Overflow en el contenedor interno */}{" "}
               {animes.map((anime, index) => (
                 <AnimeCard key={index} anime={anime} />
               ))}
